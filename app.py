b925379dee21bb64752ba848b6021975d350791e
root <root@ip-172-31-36-231.us-east-2.compute.internal>
2018-04-22 22:46:38 +0000

    User profile and other minor changes

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
+# Import all necessary packages
 from flask import Flask, session, render_template, json, request, redirect, url_for, escape
 from flaskext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
 from datetime import datetime
 import requests
 from random import shuffle
+from werkzeug.utils import secure_filename
+import os
+import collections
 
+# Internal app settings
+UPLOAD_FOLDER = 'static/tmp'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+# Initialize app and database connection
 mysql = MySQL() 
 app = Flask(__name__)
 
@@ -14,28 +23,47 @@ app.config['MYSQL_DATABASE_USER'] = 'panda'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'coinpanda'
 app.config['MYSQL_DATABASE_DB'] = 'CoinPandaDB'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 mysql.init_app(app)
 
-#  ---- App settings ----- #
+#  App data settings 
 appCoins = {'BTC' : 'Bitcoin',
-'ETH' : 'Ethereum'
+'ETH' : 'Ethereum',
+'LTC' : 'LiteCoin',
+'XRP' : 'Ripple'
 }
-appExchanges = ['Kraken','Bitstamp','Cexio','Coinbase','Exmo']
+appExchanges = ['Bitstamp','Cexio','Coinbase','Exmo','Kraken']
+appCoinsExchanges = {'BTC' : ['Kraken','Bitstamp','Cexio','Coinbase','Exmo'],
+'ETH' : ['Kraken','Bitstamp'],
+'LTC' : ['Kraken','Bitstamp','Cexio','Coinbase','Exmo'],
+'XRP' : ['Kraken','Bitstamp','Cexio']
+}
 appCurrencies = ['USD','EUR']
+appCoins = collections.OrderedDict(sorted(appCoins.items()))
+appWebsites = {'Kraken' : 'https://www.kraken.com/',
+'Bitstamp' : 'https://www.bitstamp.net/',
+'Cexio' : 'https://cex.io/',
+'Exmo' : 'https://exmo.com/',
+'Coinbase' : 'https://www.coinbase.com/'
+}
 # --- END ------#
 
 class ServerError(Exception):pass
 
+# This is the home page url for the website
+# Default landing page requests are handled here
 @app.route('/')
 def main():
     return render_template('index.html',title='CoinPanda | Home')
 
+# This function handles Contact us page url route
 @app.route('/contact')
 def contact():
     return render_template('ContactUs.html',title='CoinPanda | Contact Us')
 
+# This function handles currency comparison page requests
 @app.route('/compare')
 def compare():
     if 'name' not in session:
@@ -73,21 +101,23 @@ def compare():
 
                 graphRawData.append(tmp)
                 graphCols.append(e)
-        
+                
         for x in range(1,8):
             tmp =[x]
-            for i,e in enumerate(appExchanges):
+            for i,e in enumerate(graphCols):
                 tmp.append(graphRawData[i][x-1])
 
             graphData.append(tmp)
 
     return render_template('compare.html',coins=appCoins,minExc=minExc,coin=_coin,currency=_currency,graphData = json.dumps(graphData),graphCols = graphCols,title='CoinPanda | Compare Currency')
 
+# View the details of the recommended exchange for the selected cryptocurrency using this page.
 @app.route('/currency_specific')
 def currency_specific():
     if 'name' not in session:
         return redirect(url_for('login'))
 
+    # Access all form values
     _coin = request.args.get('coin')
     _coinName = appCoins.get(_coin)
     _exc = request.args.get('exc')
@@ -109,8 +139,12 @@ def currency_specific():
                 graphData.append([i+1,obj['close']])
 
         shuffle(appExchanges)
-    return render_template('currency_specific.html',coinName=_coinName,exchanges= appExchanges,todayData=todayData,coin=_coin,exc = _exc,currency=_currency,graphData = json.dumps(graphData),title='CoinPanda | Currency Specific')
+        appWebsite = appWebsites.get(_exc,'/')
 
+    return render_template('currency_specific.html',appWebsite=appWebsite,coinName=_coinName,exchanges= appExchanges,todayData=todayData,coin=_coin,exc = _exc,currency=_currency,graphData = json.dumps(graphData),title='CoinPanda | Currency Specific')
+
+# This is the first page visible to the user containing
+# the dashboard / investment portfolio of the user.
 @app.route('/dashboard')
 def dashboard():
     if 'name' not in session:
@@ -133,15 +167,15 @@ def dashboard():
     conn = mysql.connect()
     cur = conn.cursor()
     
-    # Widget-1
-    cur.execute("SELECT Cryptocurrency,Currency,Source,CurrentRate,Details,Symbol FROM tblCurrency ORDER BY Cryptocurrency, CurrentRate ASC;")
+    # Widget-1 data
+    cur.execute("SELECT Cryptocurrency,Currency,Source,CurrentRate,Details,Symbol FROM tblCurrency ORDER BY Cryptocurrency ASC, Currency DESC;")
 
     for row in cur.fetchall():
         obj ={'Cryptocurrency' : row[0], 'Symbol' : row[5], 'Currency' : row[1], 'Source' : row[2], 
         'CurrentRate' : row[3], 'Details': json.loads(str(row[4]))}
         data1.append(obj)
 
-    # Widget-2,3,4
+    # Widget-2,3,4 data
     cur.execute("""
         SELECT SUM(i.InvestmentValue) as TotalInvestment 
         , SUM(i.InvestmentVolume * c.CurrentRate) as NetWorth
@@ -152,18 +186,18 @@ def dashboard():
         """, format(uid))
     data2 = cur.fetchone()
 
-    # Widget 5
+    # Widget 5 data
     cur.execute("""
         SELECT UID, Cryptocurrency, SUM(InvestmentValue)
         FROM tblInvestment
-        GROUP BY Cryptocurrency
+        GROUP BY Cryptocurrency,UID
         HAVING UID=%s;
         """, format(uid))
     
     for row in cur.fetchall():
         data3.append(row)
 
-    # Widget 6
+    # Widget 6 data
     cur.execute("""
         SELECT i.Cryptocurrency, (i.InvestmentVolume * c.CurrentRate)- (i.InvestmentValue) as Profit
         FROM tblInvestment i
@@ -174,7 +208,7 @@ def dashboard():
     for row in cur.fetchall():
         data4.append(row)
 
-    # Widget-7
+    # Widget-7 data
     cur.execute("""
         SELECT c.Cryptocurrency
         , c.Symbol
@@ -198,11 +232,11 @@ def dashboard():
     for row in cur.fetchall():
         data5.append(row)
 
-    #data3 = json.dumps(data3)
     conn.close()
-    return render_template('home.html',coins=appCoins,currencies=appCurrencies,exchanges= appExchanges,data1=data1,data2=data2,data3=data3,data4=data4,data5=data5,title='CoinPanda | My Investment Portfolio')
 
-@app.route('/test')
+    return render_template('home.html',coins=appCoins,currencies=appCurrencies,appCoinsExchanges= json.dumps(appCoinsExchanges),exchanges= appExchanges,data1=data1,data2=data2,data3=data3,data4=data4,data5=data5,title='CoinPanda | My Investment Portfolio')
+
+# This function is used to refresh/update cryptocurrencies data in an interval of 15 mins
 def update_currencies():
     baseUrl = "https://min-api.cryptocompare.com/data/pricemultifull?"
     resp = []
@@ -216,7 +250,6 @@ def update_currencies():
         if 'Response' in r and r['Response'] == "Error":
             pass
         else:
-            #resp.append(r['RAW'])
             resObj = r['RAW']
             for _coin in appCoins:
                 _detObj = resObj.get(_coin,{})
@@ -239,10 +272,9 @@ def update_currencies():
                     conn.commit()
                     conn.close()
         
-        
-
     return json.dumps({'error': 0, 'message' : ''})
 
+# This function is used to ADD new investments or EDIT existing investments for a given user 
 @app.route('/save_investment/<edit>',methods=['POST'])
 def save_investment(edit):
     if 'name' not in session:
@@ -251,6 +283,8 @@ def save_investment(edit):
         if request.method == 'POST':
             conn = mysql.connect()
             cur = conn.cursor()
+
+            # Access all form values
             _coin = request.form['coin']
             _exchange = request.form['exchange']
             _currency = request.form['currency']
@@ -263,6 +297,7 @@ def save_investment(edit):
             # validate the received values
             if _coin and _exchange and _currency and _units and _value_unit and _purchased_date:   
                 if edit == "1":
+                    # Get and verify investment id
                     _invId = request.form['invId']
                     cur.execute("SELECT COUNT(1) FROM tblInvestment WHERE IID = %s;", format(_invId))
 
@@ -270,6 +305,7 @@ def save_investment(edit):
                         conn.close()
                         return json.dumps({'error': 1, 'message' : ''})
 
+                    # Update investment with id
                     cur.execute("""
                     UPDATE tblInvestment SET 
                     InvestmentValue = %s,
@@ -294,6 +330,7 @@ def save_investment(edit):
                         conn.close()
                         return json.dumps({'error': 1, 'message' : ''})
 
+                    # Create new record in the investment table
                     cur.execute("""
                     INSERT INTO tblInvestment(UID,CID,Cryptocurrency,Currency,InvestmentValue,
                     InvestmentVolume,InvestmentDate,InvestmentRate,InsertDate,UpdateDate) 
@@ -314,6 +351,7 @@ def save_investment(edit):
 
     conn.close()
 
+# This function is used to DELETE existing investments for a given user 
 @app.route('/delete_investment/',methods=['POST'])
 def delete_investment():
     if 'name' not in session:
@@ -325,13 +363,17 @@ def delete_investment():
             
             _invId = request.form['invId']
 
+            # Verify if record exists in the investment table
             cur.execute("SELECT COUNT(1) FROM tblInvestment WHERE IID = %s;", format(_invId))
 
             if not cur.fetchone()[0]:
                 conn.close()
                 return json.dumps({'error': 1, 'message' : ''})
 
+            # Delete record from the investment table
             cur.execute("DELETE FROM tblInvestment WHERE IID = %s;", format(_invId))
+            
+            # Commit changes and close DB connection
             conn.commit()
             conn.close()
 
@@ -345,6 +387,7 @@ def delete_investment():
 
     conn.close()
 
+# This page lets the user VIEW and EDIT user profile details
 @app.route('/profile', methods=['GET', 'POST'])
 def profile():
     if 'name' not in session:
@@ -357,9 +400,49 @@ def profile():
         cur = conn.cursor()
 
         if request.method == 'POST':
-             return json.dumps(request.form)
-    
-        # Widget-1
+            # Upload user image from file
+            if 'Image' in request.files:
+                file = request.files['Image']
+                if file and allowed_file(file.filename):
+                    # Upload user image and update record 
+                    filename = secure_filename(file.filename)
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    
+                    cur.execute("""
+                        UPDATE tblUser SET 
+                        Image = %s
+                        WHERE UID= %s;
+                        """,(filename,uid,))
+            else:
+                # Update details in the user record table
+                cur.execute("""
+                        UPDATE tblUser SET 
+                        FName = %s,
+                        Lname= %s,
+                        Company= %s,
+                        Address1= %s,
+                        Address2= %s,
+                        TimeZone = %s
+                        WHERE UID = %s;
+                        """, (request.form['FName'],request.form['LName'],request.form['Company'],
+                        request.form['Address1'],request.form['Address2'],request.form['TimeZone'],
+                        uid,))
+            
+                # update password
+                _password = request.form['Password']
+                _passwordm = request.form['PasswordMatch']
+                if _passwordm and _password and _passwordm!='' and _password!='':
+                    _hashed_password = generate_password_hash(_password)
+                    cur.execute("UPDATE tblUser SET Password = %s WHERE UID = %s;", (_hashed_password,uid))
+        
+        # Commit changes and close DB connection
+        conn.commit()
+        conn.close()
+
+        # Fetch user profile record
+        conn = mysql.connect()
+        cur = conn.cursor()
+
         cur.execute("""SELECT FName,Lname,Email,Company,Address1,
             Address2,TimeZone,Image
             FROM tblUser
@@ -373,10 +456,15 @@ def profile():
 
     conn.close()
 
-    #return json.dumps(userData)
     return render_template('edit.html',userData=userData,title='CoinPanda | Edit Profile')
 
-#   Login
+# Converts the name of the uploaded file to escape space and other characters
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# This function is used for to displaying user login page and
+# verifying user credentials to authenticate user
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if 'name' in session:
@@ -390,6 +478,8 @@ def login():
 
         if request.method == 'POST':
             email_form  = request.form['email']
+
+            # Check if user email exists in the database
             cur.execute("SELECT COUNT(1) FROM tblUser WHERE Email = %s;", format(email_form))
 
             if not cur.fetchone()[0]:
@@ -400,6 +490,7 @@ def login():
             cur.execute("SELECT Password,FName,LName,UID FROM tblUser WHERE Email = %s;", format(email_form))
 
             for row in cur.fetchall():
+                # Verfify user login credential for login
                 if check_password_hash(row[0],password_form):
                     session['email'] = request.form['email']
                     session['name'] = row[1] + " " + row[2]
@@ -416,16 +507,17 @@ def login():
     conn.close()
     return render_template('login.html',log_error=3,title='CoinPanda | Login')
 
-# Logout route
+# This function used to implement the Logout functionality
 @app.route('/logout')
 def logout():
+    # Clear all session variables
     session.pop('email', None)
     session.pop('name', None)
     session.pop('uid', None)
 
     return redirect(url_for('login'))
 
-# Register route
+# This function is used for to displaying user signup page and create new user record in DB
 @app.route('/register',methods=['POST','GET'])
 def signUp():
     try:
@@ -433,6 +525,7 @@ def signUp():
             conn = mysql.connect()
             cur = conn.cursor()
 
+            # Get all form values
             _fname = request.form['fName']
             _lname = request.form['lName']
             _email = request.form['Email']
@@ -445,6 +538,7 @@ def signUp():
                 cur.execute("INSERT INTO tblUser(FName,LName,Email,Password) values (%s,%s,%s,%s)", 
                 (_fname,_lname, _email, _hashed_password))
                 
+                # Commit changes and close DB connection
                 conn.commit()
                 conn.close()
 
@@ -466,5 +560,6 @@ def signUp():
 def error(e):
     return render_template('404.html',title='CoinPanda | ERROR')
 
+# Lanch the app on port 8080
 if __name__ == "__main__":
     app.run(port=8080, host='0.0.0.0',debug=True,threaded=True)
